5fb45320286047bb5bf5d8fbd473c1b15ba16782
Vadym Havryshko <Vadym.Havryshko@infopulse.com>
2024-08-16 18:00:42 +0300

    Tune HW 3

--- Infopulse/Homework3.js
+++ Infopulse/Homework3.js
@@ -1,77 +1,87 @@
 // 1. Write a program that checks whether a number is positive, negative, or zero using the if...else statement
 
-// let number = -13456;
+console.log("1. Write a program that checks whether a number is positive, negative, or zero using the if...else statement");
 
-// if (number > 0) console.log("Number is positive");
-// else if (number < 0) console.log("Number is negative");
-// else if (number = 0)console.log("Number is zero");
-// else console.log("Entered value is not number");
+let number = -13456;
 
-// console.log("-------------------------------------------");
+if (number > 0) console.log("Number is positive");
+else if (number < 0) console.log("Number is negative");
+else if (number = 0)console.log("Number is zero");
+else console.log("Entered value is not number");
+
+console.log("-------------------------------------------");
 
 // 2. Use a for loop to print the multiplication table for numbers from 1 to 10.
 
-// let result;
+console.log("2. Use a for loop to print the multiplication table for numbers from 1 to 10.");
 
-// for (let i = 1; i <= 10; i++){
+let result;
 
-// console.log("-----------");
+for (let i = 1; i <= 10; i++){
 
-//     for (let j = 1; j <= 10; j++){
+console.log("-----------");
 
-//         result = i * j;
-//         console.log(`${i} * ${j} = ${result}`);
-//     }
-// }
+    for (let j = 1; j <= 10; j++){
 
-// console.log("-------------------------------------------");
+        result = i * j;
+        console.log(`${i} * ${j} = ${result}`);
+    }
+}
+
+console.log("-------------------------------------------");
 
 // 3. Write a program that uses a while loop to print all even numbers from 1 to 20.
 
-// let evenNumber = 1;
+console.log("3. Write a program that uses a while loop to print all even numbers from 1 to 20.");
 
-// while (evenNumber <= 20){
+let evenNumber = 1;
 
-//     if (evenNumber % 2 == 0) console.log(`The number ${evenNumber} is even`);
-//     evenNumber++;
-// }
+while (evenNumber <= 20){
 
-// console.log("-------------------------------------------");
+    if (evenNumber % 2 == 0) console.log(`The number ${evenNumber} is even`);
+    evenNumber++;
+}
+
+console.log("-------------------------------------------");
 
 // 4. Use a switch statement to print the name of the day of the week based on a number (1 for Monday, 7 for Sunday).
 
-// let monthIndex = 5;
+console.log("4. Use a switch statement to print the name of the day of the week based on a number (1 for Monday, 7 for Sunday).");
 
-// switch (monthIndex) {
+let monthIndex = 5;
 
-//     case 1:
-//         console.log("You chose Monday");
-//         break;
-//     case 2:
-//         console.log("You chose Tuesday");
-//         break;
-//     case 3:
-//         console.log("You chose Wednesday");
-//         break;
-//     case 4:
-//         console.log("You chose Thursday");
-//         break;
-//     case 5:
-//         console.log("You chose Friday");
-//         break;
-//     case 6:
-//         console.log("You chose Saturday");
-//         break;
-//     case 7:
-//         console.log("You chose Sunday");
-//         break;
-//     default:
-//         console.log(`You chose wrong day index ${monthIndex}`);
-// }
+switch (monthIndex) {
 
-// console.log("-------------------------------------------");
+    case 1:
+        console.log("You chose Monday");
+        break;
+    case 2:
+        console.log("You chose Tuesday");
+        break;
+    case 3:
+        console.log("You chose Wednesday");
+        break;
+    case 4:
+        console.log("You chose Thursday");
+        break;
+    case 5:
+        console.log("You chose Friday");
+        break;
+    case 6:
+        console.log("You chose Saturday");
+        break;
+    case 7:
+        console.log("You chose Sunday");
+        break;
+    default:
+        console.log(`You chose wrong day index ${monthIndex}`);
+}
+
+console.log("-------------------------------------------");
 
 // 5. Write a program that uses a do...while loop to calculate the sum of numbers from 1 to 100.
+
+console.log("5. Write a program that uses a do...while loop to calculate the sum of numbers from 1 to 100.");
 
 let numbersSum  = 0;
 let i = 1;
